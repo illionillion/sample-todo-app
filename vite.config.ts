0df485378edc0826b32a5e56de960f377e625e86
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-05 13:26:02 +0900

    fix: 修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES ? '/sample-todo-app/' : './',
   build: {
     outDir: "docs"
   }
