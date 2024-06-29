a9d0a46809cf21283d5ee047c99ad399a86c282a
yuto ichinose <yuto.0526.sano@gmail.com>
2024-06-29 15:10:38 +0900

    fix: formated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/sample-todo-app/' : './',
+  base: process.env.GITHUB_PAGES ? "/sample-todo-app/" : "./",
   build: {
-    outDir: "docs"
-  }
-})
+    outDir: "docs",
+  },
+});
